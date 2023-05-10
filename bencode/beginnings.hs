4715d95fca222c42ceb0ed665d35299ed122cdee
Ben Rosebery <benrosebery@gmail.com>
2023-05-10 05:04:33 -0400

    update

---
+++ bencode/beginnings.hs
@@ -0,0 +1,9 @@
+--import base
+
+-- this is going to be a haskell based domain specific languge for a functional shell
+-- it will be called bsh because that's like bash and the aim is to replace bash
+--
+
+bsh :: IO ()
+
+
