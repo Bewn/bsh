fe2a43648e7eaf477a0d670fe22830275372a9d3
Ben Rosebery <benrosebery@gmail.com>
2023-05-10 06:26:27 -0400

    update

--- bencode/beginnings.hs
+++ bencode/beginnings.hs
@@ -1,9 +1,20 @@
---import base
+#!/usr/bin/env stack
+{- stack script --resolver lts-20.20
+   --package process
+   --package hashtables
+   --package haskeline
+ -}
+
+import qualified Data.HashTable.IO as Ht
+import System.Console.Haskeline
+import System.Console.Haskeline.IO as Hlio
+import Control.Concurrent
 
 -- this is going to be a haskell based domain specific languge for a functional shell
 -- it will be called bsh because that's like bash and the aim is to replace bash
---
 
-bsh :: IO ()
+main :: IO ()
+main = putStrLn "hello bish"
 
+type HashTable k v = Ht.BasicHashTable k v
 
